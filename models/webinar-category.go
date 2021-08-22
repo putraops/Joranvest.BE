a3195d1b90f729fe4c084978a93ab33a1595c4b4
Putra Ompusunggu <putraops@gmail.com>
2021-08-22 14:22:33 +0700

    Model and View Model
    Update webinar_category_id into parent_id

--- models/webinar-category.go
+++ models/webinar-category.go
@@ -5,22 +5,20 @@ import (
 )
 
 type WebinarCategory struct {
-	Id                string       `gorm:"type:varchar(50);primary_key" json:"id"`
-	IsActive          bool         `gorm:"type:bool;default:1" json:"is_active"`
-	IsLocked          bool         `gorm:"type:bool" json:"is_locked"`
-	IsDefault         bool         `gorm:"type:bool" json:"is_default"`
-	CreatedAt         sql.NullTime `gorm:"type:timestamp" json:"created_at"`
-	CreatedBy         string       `gorm:"type:varchar(50)" json:"created_by"`
-	UpdatedAt         sql.NullTime `gorm:"type:timestamp" json:"updated_at"`
-	UpdatedBy         string       `gorm:"type:varchar(50)" json:"updated_by"`
-	ApprovedAt        sql.NullTime `gorm:"type:timestamp;default:null" json:"approved_at"`
-	ApprovedBy        string       `gorm:"type:varchar(50)" json:"approved_by"`
-	EntityId          string       `gorm:"type:varchar(50);null" json:"entity_id"`
-	Name              string       `gorm:"type:varchar(50);unique" json:"name"`
-	WebinarCategoryId string       `gorm:"type:varchar(50);" json:"webinar_category_id"`
-	Description       string       `gorm:"type:text" json:"description"`
-
-	WebinarCategory Emiten `gorm:"foreignkey:WebinarCategoryId;constraint:onUpdate:CASCADE,onDelete:CASCADE" json:"parent"`
+	Id          string       `gorm:"type:varchar(50);primary_key" json:"id"`
+	IsActive    bool         `gorm:"type:bool;default:1" json:"is_active"`
+	IsLocked    bool         `gorm:"type:bool" json:"is_locked"`
+	IsDefault   bool         `gorm:"type:bool" json:"is_default"`
+	CreatedAt   sql.NullTime `gorm:"type:timestamp" json:"created_at"`
+	CreatedBy   string       `gorm:"type:varchar(50)" json:"created_by"`
+	UpdatedAt   sql.NullTime `gorm:"type:timestamp" json:"updated_at"`
+	UpdatedBy   string       `gorm:"type:varchar(50)" json:"updated_by"`
+	ApprovedAt  sql.NullTime `gorm:"type:timestamp;default:null" json:"approved_at"`
+	ApprovedBy  string       `gorm:"type:varchar(50)" json:"approved_by"`
+	EntityId    string       `gorm:"type:varchar(50);null" json:"entity_id"`
+	Name        string       `gorm:"type:varchar(50);unique" json:"name"`
+	ParentId    string       `gorm:"type:varchar(50);" json:"parent_id"`
+	Description string       `gorm:"type:text" json:"description"`
 }
 
 func (WebinarCategory) TableName() string {
