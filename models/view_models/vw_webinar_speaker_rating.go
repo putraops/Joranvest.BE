0fd3a2ed4f0d58a6a902c29453a007ba8e49b73c
Putra Ompusunggu <putraops@gmail.com>
2022-01-19 19:44:09 +0700

    update

--- models/view_models/vw_webinar_speaker_rating.go
+++ models/view_models/vw_webinar_speaker_rating.go
@@ -3,14 +3,18 @@ package view_models
 import "database/sql"
 
 type WebinarSpeakerRatingViewModel struct {
-	Id               string       `json:"id"`
-	CreatedAt        sql.NullTime `json:"created_at"`
-	SpeakerId        string       `json:"speaker_id"`
-	UserId           string       `json:"user_id"`
-	ObjectRatedId    string       `json:"object_rated_id"`
-	ReferenceId      string       `json:"reference_id"`
-	Rating           int          `json:"rating"`
-	Comment          string       `json:"comment"`
-	OrganizationName string       `json:"organization_name"`
-	SpeakerFullname  string       `json:"speaker_fullname"`
+	Id                        string       `json:"id"`
+	CreatedAt                 sql.NullTime `json:"created_at"`
+	SpeakerId                 string       `json:"speaker_id"`
+	UserId                    string       `json:"user_id"`
+	ObjectRatedId             string       `json:"object_rated_id"`
+	ReferenceId               string       `json:"reference_id"`
+	Rating                    int          `json:"rating"`
+	Comment                   string       `json:"comment"`
+	OrganizationName          string       `json:"organization_name"`
+	OrganizationFilepath      string       `json:"organization_filepath"`
+	OrganizationFilepathThumb string       `json:"organization_filepath_thumb"`
+	SpeakerFullname           string       `json:"speaker_fullname"`
+	SpeakerFilepath           string       `json:"speaker_filepath"`
+	SpeakerFilepathThumb      string       `json:"speaker_filepath_thumb"`
 }
